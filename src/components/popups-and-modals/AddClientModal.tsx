d99ddcbd2c53fb0625b1e65a83443ef5b4775298
Pavel Petrov <doctorrosen2016@gmail.com>
2022-11-15 23:03:19 +0600

    some stuff

--- src/components/popups-and-modals/AddClientModal.tsx
+++ src/components/popups-and-modals/AddClientModal.tsx
@@ -5,7 +5,7 @@ import { Button, Modal, Portal, Provider, Text } from 'react-native-paper';
 export const AddClientModal = ({ visible, hide }) => {
   const containerStyle = {
     backgroundColor: 'white',
-    padding: 20,
+    padding: 10,
   };
 
   return (
@@ -15,7 +15,7 @@ export const AddClientModal = ({ visible, hide }) => {
         dismissable={false}
         contentContainerStyle={containerStyle}
       >
-        <Text>Example Modal. Click outside this area to dismiss.</Text>
+        <Text variant='titleLarge'>Введите данные нового клиента</Text>
         <View style={styles.bottomContainer}>
           <Button
             onPress={hide}
@@ -32,8 +32,11 @@ export const AddClientModal = ({ visible, hide }) => {
 };
 
 const styles = StyleSheet.create({
+  titleBlock: {
+    paddingBottom: 10
+  },
   bottomContainer: {
-    paddingTop: 40,
+    paddingTop: 30,
     paddingBottom: 10
   }
 });
