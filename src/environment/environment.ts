27ff624ddcc87ef9370a0b31d01ac6fb416e26a1
Pavel Petrov <doctorrosen2016@gmail.com>
2023-09-23 11:53:19 +0600

    added environment.ts

---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aHliemVpa2JqaWp5aHdmdG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYxMTU1MDYsImV4cCI6MTk4MTY5MTUwNn0.TbbHSnGzy1AD6Gr-9G1QYn2m0AmMn-vOkA5iM8QjqvY',
+  supabaseUrl: 'https://szhybzeikbjijyhwftnn.supabase.co'
+};
