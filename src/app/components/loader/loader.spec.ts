805578ef0501f94628163b7753603c7164144d25
Pavel Petrov <doctorrosen2016@gmail.com>
2024-01-06 14:57:03 +0600

    all tests has been removed

--- src/app/components/loader/loader.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoaderComponent } from './loader.component';
-
-describe('Loader-component testing for', () => {
-  it('initializing', () => {
-    let component = new LoaderComponent();
-    expect(component).toBeDefined();
-  })
-})
