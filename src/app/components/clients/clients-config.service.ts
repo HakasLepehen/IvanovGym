be0a8400ec361a1cbe9b161dea9446bbe3223af2
Pavel Petrov <doctorrosen2016@gmail.com>
2024-01-21 18:08:46 +0600

    some refactoring part #1

--- src/app/components/clients/clients-config.service.ts
+++ src/app/components/clients/clients-config.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 import { TuiDialogContext, TuiDialogService } from '@taiga-ui/core';
 import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, of, takeUntil, catchError, Observable, ObservableInput, OperatorFunction, tap } from 'rxjs';
 import { IClient } from 'src/app/interfaces/client';
 import IClientDialog from 'src/app/interfaces/client-dialog';
 import { ClientsService } from 'src/app/services/clients/clients.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
 import { ClientOperationsComponent } from '../client-operations/client-operations.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -31,14 +32,14 @@ export class ClientsConfigService {
 
   getClients(): void {
     this.loader.show();
-    this.cs.getClients().subscribe({
-      error: (err: any) => {
-        console.log(err);
-        this.loader.hide();
-        alert('Не удалось создать клиента, обратитесь к разработчику');
-      },
-      complete: () => this.loader.hide(),
-    });
+    this.cs.getClients()
+      .pipe(
+        tap(() => { this.loader.hide() }),
+        catchError((err: any): Observable<any> => {
+          return this.handleError(err.message);
+        }),
+      )
+      .subscribe();
   }
 
   //TODO: не сделана логика обновления списка пользователя
@@ -50,10 +51,10 @@ export class ClientsConfigService {
           client: el
             ? el
             : {
-                fullName: '',
-                created_at: new Date(),
-                age: 0,
-              },
+              fullName: '',
+              created_at: new Date(),
+              age: 0,
+            },
           isEdit: !!el,
         },
         closeable: true,
@@ -70,59 +71,61 @@ export class ClientsConfigService {
     this.loader.show();
     this.cs
       .createClient(data)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        error: (err: any) => {
-          console.log(err);
-          this.loader.hide();
-          this.refreshData();
-          alert('Не удалось создать клиента, обратитесь к разработчику');
-        },
-        complete: () => {
-          this.loader.hide();
-          context.completeWith(true);
-          this.refreshData();
-        },
-      });
+      .pipe(
+        tap(this.closeModal(context)),
+        catchError((err: any): Observable<any> => {
+          return this.handleError(err.message);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
   removeClient(guid: string): void {
     this.loader.show();
     this.cs
       .removeClient(guid)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        error: (err: any) => {
-          console.log(err);
-          this.loader.hide();
-          this.refreshData();
-          alert('Не удалось удалить клиента, обратитесь к разработчику');
-        },
-        complete: () => {
-          this.loader.hide();
-          this.refreshData();
-        },
-      });
+      .pipe(
+        tap(this.hideLoaderAndRefresh()),
+        catchError((err: any): Observable<any> => {
+          return this.handleError(err.message);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
   editClient(model: IClient, context: TuiDialogContext<boolean, IClientDialog>): void {
     this.loader.show();
     this.cs
       .editClient(model)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        error: (err: any) => {
-          console.log(err);
-          this.loader.hide();
-          this.refreshData();
-          alert('Не удалось создать клиента, обратитесь к разработчику');
-        },
-        complete: () => {
-          this.loader.hide();
-          context.completeWith(true);
-          this.refreshData();
-        },
-      });
+      .pipe(
+        tap(this.closeModal(context)),
+        catchError((err: any) => {
+          return this.handleError(err.message)
+        }),
+        takeUntil(this.destroy$))
+      .subscribe();
+  }
+
+  hideLoaderAndRefresh(): Observable<never> {
+    this.loader.hide();
+    this.refreshData();
+    return of();
+  }
+
+  closeModal(context: TuiDialogContext<boolean, IClientDialog>): Observable<never> {
+    this.hideLoaderAndRefresh();
+    context.completeWith(true);
+    return of();
+  }
+
+  handleError(msg: string): Observable<never> {
+    console.log(msg);
+    this.loader.hide();
+    this.refreshData();
+    alert(msg);
+    return of();
   }
 
   refreshData() {
