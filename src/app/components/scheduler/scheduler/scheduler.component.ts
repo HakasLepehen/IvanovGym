c79a0235e562f4d4cb9a738052a44880e88c477c
Pavel Petrov <doctorrosen2016@gmail.com>
2024-08-19 22:11:39 +0600

    added styles

--- src/app/components/scheduler/scheduler/scheduler.component.ts
+++ src/app/components/scheduler/scheduler/scheduler.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TuiDay } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-scheduler',
   templateUrl: './scheduler.component.html',
-  styleUrls: ['./scheduler.component.scss']
+  styleUrls: ['./scheduler.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SchedulerComponent {
+export class SchedulerComponent implements OnInit, OnChanges {
   public value: TuiDay | null = null;
+
+  constructor() {
+    console.log('constructor started');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges started');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit started');
+  }
 
   public onDayClick(day: TuiDay | any): void {
     this.value = day;
--- src/app/components/training-calendar-list/training-calendar-list.component.ts
+++ src/app/components/training-calendar-list/training-calendar-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, Component, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TUI_DEFAULT_MATCHER, TuiDay } from '@taiga-ui/cdk';
 import { Subject } from 'rxjs/internal/Subject';
 import { map, startWith, switchMap } from 'rxjs/operators';
 import { ITraining } from 'src/app/interfaces/training';
+import { TIMELINES } from './timeline';
 
 @Component({
   selector: 'app-training-calendar-list',
   templateUrl: './training-calendar-list.component.html',
-  styleUrls: ['./training-calendar-list.component.scss']
+  styleUrls: ['./training-calendar-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TrainingCalendarListComponent {
+export class TrainingCalendarListComponent implements OnInit, OnChanges, AfterViewInit, AfterContentInit {
   @Input()
   public selectedDay: TuiDay | null = null;
+  public timeline: string[] = TIMELINES;
   public content: ITraining[] = [
     {
       clientGIUD: '123',
@@ -22,7 +25,19 @@ export class TrainingCalendarListComponent {
     }
   ];
 
-  constructor() {}
+  constructor(private _hostElement: ElementRef) {
+    console.log('constructor started: ', _hostElement.nativeElement);
+  }
+  ngAfterContentInit(): void {
+    console.log('constructor started: ', this._hostElement.nativeElement);
+  }
+  ngAfterViewInit(): void {
+    console.log('constructor started: ', this._hostElement.nativeElement);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges started');
+  }
 
   ngOnInit() {
     console.log(this.selectedDay);
