bff9b28632c93efcfd546e097be4a8ffd512a209
petrovpv <petrovpv@competentit.ru>
2025-12-12 10:55:57 +0600

    completed

--- src/app/components/training-exercise-item/training-exercise-item.component.ts
+++ src/app/components/training-exercise-item/training-exercise-item.component.ts
@@ -6,17 +6,18 @@ import {
   Input,
   OnChanges,
   Output,
-  SimpleChanges
+  SimpleChanges,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { clientExercisesSelector } from '../../store/selectors/client-exercises.selector';
-import { map, take } from 'rxjs';
+import { map, of, take } from 'rxjs';
 import IClientExercise from '../../interfaces/client_exercise';
 import { TuiComboBoxModule, TuiSelectModule, TuiTextareaModule } from '@taiga-ui/legacy';
 import { ControlContainer, FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiButton, TuiDataListDirective, TuiLabel, TuiTextfield, TuiTextfieldComponent } from '@taiga-ui/core';
 import { TuiDataListWrapperComponent, tuiItemsHandlersProvider } from '@taiga-ui/kit';
 import { BodyParts } from '../../enums/body_parts';
+import { tap } from 'rxjs/internal/operators/tap';
 
 @Component({
   selector: 'app-training-exercise-item',
@@ -33,14 +34,14 @@ import { BodyParts } from '../../enums/body_parts';
     TuiTextareaModule,
     TuiDataListDirective,
     TuiSelectModule,
-    TuiButton
+    TuiButton,
   ],
   standalone: true,
   providers: [
     // обработчик отображения элемента в tui-select наименования Упражнения
     tuiItemsHandlersProvider({
-      stringify: (item: IClientExercise) => item.exercise_fullname as string
-    })
+      stringify: (item: IClientExercise) => item.exercise_fullname as string,
+    }),
     // tuiItemsHandlersProvider({
     //   stringify: (item: IClientExercise) => item.name as string,
     // }),
@@ -51,18 +52,25 @@ import { BodyParts } from '../../enums/body_parts';
     //   useFactory: () => inject(ControlContainer, {skipSelf: true})
     // }
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrainingExerciseItemComponent implements OnChanges {
   @Input({ required: true }) index!: number;
-  @Output() messageSent = new EventEmitter<{ id: number | string, index: number; }>(); // EventEmitter для отправки данных
+  @Output() messageSent = new EventEmitter<{ id: number | string; index: number }>(); // EventEmitter для отправки данных
   exec_vars: IClientExercise[][] = [];
   store = inject(Store);
   selectedExecVar: any;
   exForm!: FormGroup;
-  body_parts: string[] = [];
+  body_parts!: string[];
 
   constructor(private fb: FormBuilder, private controlContainer: ControlContainer) {
+    of(BodyParts)
+      .pipe(
+        take(1),
+        map((body_parts) => body_parts.map((part) => part.name)),
+        tap((result) => (this.body_parts = result))
+      )
+      .subscribe();
     this.store
       .select(clientExercisesSelector)
       .pipe(
@@ -70,16 +78,15 @@ export class TrainingExerciseItemComponent implements OnChanges {
         map((exercises) => {
           const result: IClientExercise[][] = new Array(BodyParts.length);
           exercises.forEach((exercise: IClientExercise, index: number) => {
-            result[index] = []
+            result[index] = [];
             exercise.body_part_ids?.forEach((id) => {
-              if (!result[index]) result[index] = []
+              if (!result[id]) result[id] = [];
 
               result[id].push(exercise);
-              console.log(result);
-            })
-          })
+            });
+          });
           return result;
-        }),
+        })
       )
       // Не обязательно каждый раз перечитывать упражнения.
       // Поскольку редактировать упражнения и тренировки будет один человек
@@ -92,16 +99,15 @@ export class TrainingExerciseItemComponent implements OnChanges {
     let exercisesFormArray: FormArray<any> = this.controlContainer.control?.get('exercises') as FormArray;
     this.exForm = exercisesFormArray.at(this.index) as FormGroup;
     // инициализация поля выбора упражнения через поиск
-    this.selectedExecVar = this.exec_vars.flat().find((el: IClientExercise) => el.id === this.exForm.get('exercise')?.value);
+    this.selectedExecVar = this.exec_vars
+      .flat()
+      .find((el: IClientExercise) => el.id === this.exForm.get('exercise')?.value);
     this.exForm.get('exercise')?.setValue(this.selectedExecVar);
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
   removeExercise(): void {
     this.messageSent.emit({ id: this.exForm.get('id')?.value, index: this.index });
   }
 }
-
-
