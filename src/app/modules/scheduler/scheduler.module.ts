bc6b7a23066cf28165bcf89f048289b3b1f1996f
Pavel Petrov <doctorrosen2016@gmail.com>
2026-03-06 00:01:13 +0600

    added filter of clients

--- src/app/modules/scheduler/scheduler.module.ts
+++ src/app/modules/scheduler/scheduler.module.ts
@@ -8,16 +8,23 @@ import { LoaderModule } from "../../components/loader/loader.module";
 import { SchedulerConfigService } from 'src/app/components/scheduler/scheduler-config.service';
 import { SortByTimePipe } from '../../pipes/sort-by-time/sort-by-time.pipe';
 import { TransformMinutesPipe } from '../../pipes/transform-minutes/transform-minutes.pipe';
+import { ClientsFilterComponent } from 'src/app/components/clients-filter/clients-filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiGroup } from '@taiga-ui/core';
+import { TuiBlock, TuiRadio, TuiRadioComponent } from '@taiga-ui/kit';
 
 @NgModule({
-  declarations: [SchedulerComponent, TrainingCalendarListComponent],
+  declarations: [SchedulerComponent, TrainingCalendarListComponent, ClientsFilterComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     SchedulerRoutingModule,
     TaigaModule,
     LoaderModule,
     SortByTimePipe,
-    TransformMinutesPipe
+    TransformMinutesPipe,
+    TuiBlock, TuiGroup,
   ],
   providers: [SchedulerConfigService]
 })
